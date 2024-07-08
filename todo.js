5b34d4999b2f40f69a03168b28539a7aad91b248
Mayankyadav3980 <ymayank501@gmail.com>
2024-07-08 12:33:43 +0530

    todoapp completed

--- todo.js
+++ todo.js
@@ -1,57 +1,85 @@
 let tasks = [];
-const taskList = document.getElementById('list');
-const addTaskInput = document.getElementById('add');
-const tasksCounter = document.getElementById('tasks-counter');
+const tasksList = document.getElementById("list");
+const addTaskInput = document.getElementById("add");
+const tasksCounter = document.getElementById("tasks-counter");
 
+function renderList() {
+  tasksList.innerHTML = "";
 
-function renderList () {
-    taskList.innerHTML ='task'
+  for (let i = 0; i < tasks.length; i++) {
+    let task = tasks[i];
+    const li = document.createElement("li");
+    li.innerHTML = `<input type="checkbox" 
+              id="${task.id}" 
+              ${task.done ? "checked" : ""}  
+              class="custom-checkbox" 
+              onclick='markTaskAsComplete(${task.id})'>
+
+              <label for="${task.id}">${task.text}</label>
+              <img src="bin.png" c
+              lass="delete" 
+              id="${task.id}" 
+              onclick='deleteTask(${task.id})' />    
+             `;
+    tasksList.append(li);
+  }
+
+  tasksCounter.innerHTML = tasks.length;
 }
 
-function markTaskAsComplete (taskId) {
-    let newTasks = tasks.map( obj => {
-        if(obj.id == taskId){
-            return { ...obj, done:!(obj.done)};
-        }
-        else{
-            return obj;
-        }
-    })
-    tasks = newTasks;
-    renderList();
-    showNotification('task toggled success')
+function markTaskAsComplete(taskId) {
+  let newTasks = tasks.map((obj) => {
+    if (obj.id == taskId) return { ...obj, done: !obj.done };
+    else return obj;
+  });
+  tasks = newTasks;
+  renderList();
 }
 
-function deleteTask (taskId) {
-    let newTasks = tasks.filter(obj => obj.id !== taskId )
-    tasks = newTasks;
-    renderList();
-    showNotification('Task deleted successfully')
+function deleteTask(taskId) {
+  let newTasks = tasks.filter((obj) => obj.id != taskId);
+  tasks = newTasks;
+  renderList();
 }
 
-function addTask (task) {
-   
-    tasks.push(task);
-    showNotification('Task added successfullu')
-    renderList();
+function addTask(task) {
+  tasks.push(task);
+  renderList();
 }
 
-function showNotification(text) {
-    alert(text);
-}
-
-function handleInputKeypress(e){
-if(e.key == 'Enter'){
+function handleInputKeypress(e) {
+  if (e.key == "Enter") {
     const text = e.target.value;
-    if(!text){ showNotification('enter todo') };
-    const task = { 
-        id: Date.now().toString,
-        task,
-        done: false
+    if (!text) {
+      showNotification("enter todo");
+      return;
     }
-    e.target.value = '';
+    const task = {
+      id: Date.now(),
+      text: text,
+      done: false,
+    };
+    e.target.value = "";
     addTask(task);
+  }
 }
-}
+addTaskInput.addEventListener("keypress", handleInputKeypress);
 
-addTaskInput.addEventListener('keypress', handleInputKeypress);
+// #example of event Deligation
+
+// function handleClickListener(e){
+//     const target = e.target;
+
+//     if(target.className == 'delete'){
+//         console.log('in handle click del');
+//         const taskId = target.id;
+//         deleteTask(taskId);
+//         return;
+//     }else if (target.className == 'custom-checkbox'){
+//         const taskId = target.id;
+//         markTaskAsComplete(taskId);
+//     }
+// }
+
+// #example of event Deligation
+// document.addEventListener('click', handleClickListener);
